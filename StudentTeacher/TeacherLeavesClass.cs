0f3f9c056d031b98fb6444be94447e2db526b7ed
Jes Bak Hansen <jbh@pension.dk>
2024-04-24 22:56:29 +0200

    Add the two events
    Notice how this only specify that something happens. It doesn't do anything by itself. It just signals that something happened.

---
+++ StudentTeacher/TeacherLeavesClass.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace StudentTeacher;
+
+/// <summary>
+/// This event signals that the teacher leaves the classroom
+/// </summary>
+public class TeacherLeavesClass : INotification;
